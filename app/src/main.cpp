5573176b3584c21418900fb42edea598f541eb49
Vinicius Souza <vinicius.rsouza07@gmail.com>
2022-05-19 22:00:38 -0300

    opening window

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,8 +2,89 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int init(GLFWwindow **window);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
 int main(void)
 {
-  std::cout << "Hello, World!" << std::endl;
+  GLFWwindow *window;
+  int err = init(&window);
+  if (err != 0)
+  {
+    std::cerr << "Application failed to initialize with code: " << err << std::endl;
+    return err;
+  }
+
+  // render loop
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+    glfwPollEvents();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+  }
+
   return 0;
+}
+
+int init(GLFWwindow **window)
+{
+  if (!glfwInit())
+  {
+    std::cout << "Failed to initialize GLFW" << std::endl;
+    return -1;
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  std::cout << "Apple system, runnning in compat mode." << std::endl;
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  *window = glfwCreateWindow(800, 600, "CG Project", NULL, NULL);
+
+  if (window == nullptr)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -2;
+  }
+
+  unsigned int major = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MAJOR);
+  unsigned int minor = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MINOR);
+  std::cout << "Shader version: " << major << "." << minor << std::endl;
+
+  glfwMakeContextCurrent(*window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -3;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(*window, framebuffer_size_callback);
+
+  glEnable(GL_DEPTH_TEST);
+  glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, true);
+    std::cout << "Escape key pressed, terminating..." << std::endl;
+  }
 }
