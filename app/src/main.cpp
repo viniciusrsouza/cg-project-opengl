6adb4e355c18fec011f4ed3f15b4b0f878f48d3e
Vinicius Souza <vinicius.rsouza07@gmail.com>
2022-05-19 23:48:45 -0300

    loading window with triangle

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,6 +2,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <core/shader.h>
+
 int init(GLFWwindow **window);
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -15,6 +17,36 @@ int main(void)
     std::cerr << "Application failed to initialize with code: " << err << std::endl;
     return err;
   }
+  Shader shader("resources/shaders/basic-vertex.glsl", "resources/shaders/basic-fragment.glsl");
+
+  float vertices[] = {
+      0.5f, 0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      -0.5f, -0.5f, 0.0f,
+      -0.5f, 0.5f, 0.0f};
+
+  unsigned int indices[] = {
+      0, 1, 3,
+      1, 2, 3};
+
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   // render loop
   while (!glfwWindowShouldClose(window))
@@ -22,6 +54,11 @@ int main(void)
     processInput(window);
     glfwPollEvents();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    /* render here */
+    shader.use();
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
   }
@@ -46,16 +83,12 @@ int init(GLFWwindow **window)
 
   *window = glfwCreateWindow(800, 600, "CG Project", NULL, NULL);
 
-  if (window == nullptr)
+  if (*window == nullptr)
   {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -2;
   }
-
-  unsigned int major = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MAJOR);
-  unsigned int minor = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MINOR);
-  std::cout << "Shader version: " << major << "." << minor << std::endl;
 
   glfwMakeContextCurrent(*window);
 
@@ -65,12 +98,17 @@ int init(GLFWwindow **window)
     return -3;
   }
 
-  glViewport(0, 0, 800, 600);
+  unsigned int major = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MAJOR);
+  unsigned int minor = glfwGetWindowAttrib(*window, GLFW_CONTEXT_VERSION_MINOR);
+  std::cout << "Shader version: " << major << "." << minor << std::endl;
+
+  std::cout << "Initialized OpenGL context" << std::endl;
+  std::cout << glGetString(GL_VERSION) << std::endl;
 
   glfwSetFramebufferSizeCallback(*window, framebuffer_size_callback);
 
   glEnable(GL_DEPTH_TEST);
-  glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
   return 0;
 }
